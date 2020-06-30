30090adc51c9263a715d8019fcd2c0e712fd55db
dvkunion <2622100059@qq.com>
2020-06-30 15:54:57 +0800

    Site updated: 2020-06-30 15:54:57

--- js/mouse.js
+++ js/mouse.js
@@ -1,30 +1,30 @@
 var a_idx = 0;
 
-    $("body").click(function(e) {
-        var a = new Array(
-            "(*´∀`)~♥", "( ´ﾟДﾟ`)", "⁽⁽ଘ( ˙꒳˙ )ଓ⁾⁾", "♥(´∀` )人",
-            "(ﾟ皿ﾟﾒ)", "(=´ᴥ`)", "(|||ﾟдﾟ)", "╮(′～‵〞)╭",
-            "(ó㉨ò)", "(,,ﾟДﾟ)", "✧*｡٩(ˊᗜˋ*)و✧*｡", "(◔౪◔)"
-            );
-        var $i = $("<span/>").text(a[a_idx]);
-        a_idx = (a_idx + 1) % a.length;
-        var x = e.pageX,
+$("body").click(function (e) {
+    var a = new Array(
+        "(*´∀`)~♥", "( ´ﾟДﾟ`)", "⁽⁽ଘ( ˙꒳˙ )ଓ⁾⁾", "♥(´∀` )人",
+        "(ﾟ皿ﾟﾒ)", "(=´ᴥ`)", "(|||ﾟдﾟ)", "╮(′～‵〞)╭",
+        "(ó㉨ò)", "(,,ﾟДﾟ)", "✧*｡٩(ˊᗜˋ*)و✧*｡", "(◔౪◔)"
+    );
+    var $i = $("<span/>").text(a[a_idx]);
+    a_idx = (a_idx + 1) % a.length;
+    var x = e.pageX,
         y = e.pageY;
-        $i.css({
-            "z-index": 144469,
-            "top": y - 20,
-            "left": x,
-            "position": "absolute",
-            "font-weight": "bold",
-            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
-        });
-        $("body").append($i);
-        $i.animate({
-            "top": y - 180,
-            "opacity": 0
-        },
+    $i.css({
+        "z-index": 144469,
+        "top": y - 20,
+        "left": x,
+        "position": "absolute",
+        "font-weight": "bold",
+        "color": "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
+    });
+    $("body").append($i);
+    $i.animate({
+        "top": y - 180,
+        "opacity": 0
+    },
         1500,
-        function() {
+        function () {
             $i.remove()
         })
-    });
+});
